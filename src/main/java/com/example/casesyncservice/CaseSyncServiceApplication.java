9f2eb78f2cf97f2b698a0d610c99575cb699a484
Yashi Jaiswal <yashijaiswal411@gmail.com>
2023-10-23 10:18:09 +0530

    First Commit

---
+++ src/main/java/com/example/casesyncservice/CaseSyncServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.casesyncservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaseSyncServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaseSyncServiceApplication.class, args);
+	}
+
+}
